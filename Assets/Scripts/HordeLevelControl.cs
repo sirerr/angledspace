73d76202639d910d626569d9e7ac371494f7545a
Leonard Wedderburn <sirerr@gmail.com>
2013-12-28 22:35:43 -0500

    The Horde!

---
+++ Assets/Scripts/HordeLevelControl.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HordeLevelControl : MonoBehaviour {
+
+	private GameObject[] espawns;
+	private int randomint;
+	private int spawnamount;
+	public GameObject enemy1;
+	public GameObject enemy2;
+	private int spoint;
+	public int levelinc = 2;
+	public int levelchoice = 10;
+
+	// Use this for initialization
+	void Start () {
+		espawns = GameObject.FindGameObjectsWithTag("ERespawn");
+		spawnamount = espawns.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		int enemyamount;
+	
+
+		enemyamount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+		if( enemyamount == 0)
+		{
+
+			if(levelinc ==levelchoice)
+			{
+				for(int i=0;i<=levelinc;i++){
+					spoint = Randompoint(spoint);
+					Instantiate (enemy1,espawns[spoint].transform.position,Quaternion.identity);
+					spoint = Randompoint(spoint);
+					Instantiate (enemy2,espawns[spoint].transform.position,Quaternion.identity);
+					}
+			}
+				else 
+			{
+			levelinc++;
+
+			for(int i=0;i<=levelinc;i++){
+				spoint = Randompoint(spoint);
+			Instantiate (enemy1,espawns[spoint].transform.position,Quaternion.identity);
+				spoint = Randompoint(spoint);
+			Instantiate (enemy2,espawns[spoint].transform.position,Quaternion.identity);
+			
+				}
+			}
+		}
+	}
+
+	int Randompoint (int num1){
+
+		int ret =0;
+		ret = Random.Range (0,spawnamount);
+		return ret;
+
+	}
+
+}
