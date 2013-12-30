7efff571c8753a320cbd033070717987548c19a3
Leonard Wedderburn <sirerr@gmail.com>
2013-12-29 22:04:04 -0500

    Getting map working

---
+++ Assets/Scripts/Loadlevel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loadlevel : MonoBehaviour {
+	public int level;
+	public GUIText levelselected;
+	public GameObject lookater;
+	private float apress;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		apress = Input.GetAxis("Dash");
+
+		RaycastHit hit;
+		Ray starter = new Ray(transform.position,Vector3.up);
+
+		Physics.Raycast(starter,out hit,100f);
+	
+		if(hit.collider.gameObject == lookater)
+		{
+		
+			levelselected.text = "Press A to Start";
+
+			if(apress == 1){
+			Application.LoadLevel(level);
+			}
+		}
+		else{
+			levelselected.text = "...";
+		}
+
+	}
+}
---
+++ Assets/Scripts/MapLevelControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapLevelControl : MonoBehaviour {
+
+
+	public GUIText mapselected;
+	public GameObject map;
+	private float vert;
+	private float hor;
+ 
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		vert = Input.GetAxis("Menu2");
+		hor = Input.GetAxis ("Menu1");
+
+		if( Mathf.Abs(vert) == 1f)
+		{
+			map.transform.Rotate(vert*90f*(Time.deltaTime*2),0,0);
+		}
+		if( Mathf.Abs(vert) == -1f)
+		{
+			map.transform.Rotate(vert*90f*(Time.deltaTime*2),0,0);
+		}
+
+		if( Mathf.Abs(hor) == 1f)
+		{
+			map.transform.Rotate(0,hor*90f*(Time.deltaTime*2),0);
+		}
+		if( Mathf.Abs(hor) == -1f)
+		{
+			map.transform.Rotate(0,hor*90f*(Time.deltaTime*2),0);
+		}
+
+		RaycastHit hit;
+		Ray atmap = new Ray(transform.position,Vector3.forward);
+
+		Physics.Raycast(atmap,out hit,100f);
+		mapselected.text = hit.collider.name;
+
+	}
+}
