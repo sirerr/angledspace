49b38908a504bb85da04b2e20219c4abb2aa42db
Leonard Wedderburn <sirerr@gmail.com>
2014-01-13 22:50:04 -0500

    power up work

--- Assets/Scripts/CherControl.cs
+++ Assets/Scripts/CherControl.cs
@@ -122,6 +122,7 @@ public class CherControl : MonoBehaviour {
 	}
 	
 	IEnumerator dashCount(){
+
 		yield return new WaitForSeconds(2);
 		
 		isDash = false;
--- Assets/Scripts/WeapHit.cs
+++ Assets/Scripts/WeapHit.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class WeapHit : MonoBehaviour {
 
+	private int enemyhit;
+	private GameObject[] powerpossible;
+	private int poweruplocation;
+	private int pcounter;
+	public GameObject circle1;
+	public GameObject circle2;
+	public GameObject circle3;
+	public GameObject circle4;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		powerpossible = GameObject.FindGameObjectsWithTag("PowerP");
+		poweruplocation = powerpossible.Length;
+		pcounter = 1;
+		 
 	}
 	
 	// Update is called once per frame
@@ -15,6 +28,31 @@ public class WeapHit : MonoBehaviour {
 	
 	void OnTriggerEnter(Collider target){
 		if(target.tag == "Enemy"){
+			enemyhit++;
+			Debug.Log(enemyhit);
+			Debug.Log("pcounter" + pcounter);
+		//	if(pcounter <= poweruplocation)
+			//	{
+				switch(enemyhit)
+
+				{
+				case 5:
+				Instantiate (circle1,powerpossible[pcounter].transform.position,Quaternion.identity);
+				break;
+				case 10:
+				Instantiate (circle2,powerpossible[pcounter].transform.position,Quaternion.identity);
+				break;
+				case 15:
+				Instantiate (circle3,powerpossible[pcounter].transform.position,Quaternion.identity);
+				break;
+				case 20:
+				Instantiate (circle4,powerpossible[pcounter].transform.position,Quaternion.identity);
+					pcounter++;
+					enemyhit = 0;
+					break;
+				}
+
+			//} 
 			target.gameObject.SendMessage ("takeHit");
 		}
 	}
