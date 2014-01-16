bf9041acf34e0a3a98f56392a7d2d549c23f0d2d
Leonard Wedderburn <sirerr@gmail.com>
2014-01-15 22:19:11 -0500

    still can't get powerup to work

--- Assets/Scripts/WeapHit.cs
+++ Assets/Scripts/WeapHit.cs
@@ -5,6 +5,7 @@ public class WeapHit : MonoBehaviour {
 
 	private int enemyhit;
 	private GameObject[] powerpossible;
+	private int[] powerlevel;
 	private int poweruplocation;
 	private int pcounter;
 	public GameObject circle1;
@@ -18,9 +19,12 @@ public class WeapHit : MonoBehaviour {
 	void Start () {
 		powerpossible = GameObject.FindGameObjectsWithTag("PowerP");
 		Debug.Log (powerpossible.Length);
+		arraycount = 0;
+		powerlevel = new int[powerpossible.Length];
+		Debug.Log (powerlevel.Length);
 		poweruplocation = powerpossible.Length;
 		pcounter = 0;
-		arraycount = 0;
+
 		 
 	}
 	
@@ -32,33 +36,42 @@ public class WeapHit : MonoBehaviour {
 	void OnTriggerEnter(Collider target){
 		if(target.tag == "Enemy"){
 			enemyhit++;
+
+
 			Debug.Log(enemyhit);
-			Debug.Log("pcounter" + pcounter);
-			Debug.Log (poweruplocation);
+		//	Debug.Log("pcounter" + pcounter);
+		//	Debug.Log (poweruplocation);
 		if(pcounter < poweruplocation)
 				{
+				powerlevel[pcounter] = enemyhit;
+
 				switch(enemyhit)
 
 				{
 				case 5:
-					Instantiate (circle1,powerpossible[pcounter].transform.position,Quaternion.identity);
-					Debug.Log ("instantiate");
+				Instantiate (circle1,powerpossible[pcounter].transform.position,Quaternion.identity);
+					powerlevel[pcounter] = 5;
+	 
 				break;
 				case 10:
 				Instantiate (circle2,powerpossible[pcounter].transform.position,Quaternion.identity);
+					powerlevel[pcounter] = 10;
+				Debug.Log("powerlevel "+ pcounter);
+			//		Debug.Log(enemyhit);
 				break;
 				case 15:
 				Instantiate (circle3,powerpossible[pcounter].transform.position,Quaternion.identity);
+					powerlevel[pcounter] = 15;
 				break;
 				case 20:
 				Instantiate (circle4,powerpossible[pcounter].transform.position,Quaternion.identity);
+
+					powerlevel[pcounter] = 20;
+				//	Debug.Log(powerlevel[pcounter]);
 					pcounter++;
 					enemyhit = 0;
 					break;
-				default:
-					break;
 				}
-
 			} 
 			target.gameObject.SendMessage ("takeHit");
 		}
